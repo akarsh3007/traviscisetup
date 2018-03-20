f5cc2dca0d56ed5a52d0cc0d0d183d6b8ff7c0ae
akarsh3007 <gupta.akarsh@gmail.com>
2018-03-20 15:39:08 -0400

    travis config updated. Main file updated

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,9 @@ object Main  {
 
   def main(args: Array[String]): Unit = {
 
-      println(PersonHelper.checkPersonAgeGroup( Person("Akarsh", "Gupta", 26)))
+      println(PersonHelper.checkPersonAgeGroup( Person("Test", "User", 26)))
+
+      println(PersonHelper.checkPersonAgeGroup( Person("Test", "User1", 38)))
 
   }
 
